6be89614af02bbc3645757763948b8aa72870acc
AbhishekVerma128 <vermaabhishek128@gmail.com>
2025-11-08 19:10:01 +0530

    mongo url added

--- dbConnection/connection.js
+++ dbConnection/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb://localhost:27017/job-import").then(() => {
+    await mongoose.connect("mongodb+srv://vermaabhishek128:0155Mee002@cluster0.1exqwcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
         console.log("connected to MongoDb");
     }).catch((e) => {
         console.log("error in connect to db: " + e);
