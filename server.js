866f003f46a4ebcdc7d66d29f3a6d73d69194588
AbhishekVerma128 <vermaabhishek128@gmail.com>
2025-11-08 19:20:20 +0530

    ip added

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = require("./dbConnection/connection")
 connectDB();
-const {connectRedis} = require("./service/redis")
+const { connectRedis } = require("./service/redis")
 connectRedis()
 const routes = require("./routes/jobs.routes")
 const port = process.env.PORT || 3001;
 const app = exprss();
 app.use("/", routes)
 
-app.listen(port, () => {
+app.listen(port, "127.0.0.1", () => {
     console.log(`Server is running on port ${port}`);
 
 })
